6e904087fd057e9b261277d21fdc0e8af65d5e86
crystalDf <chendong333@gmail.com>
2016-05-05 00:10:56 +0800

    Chapter 15 Data Visualization

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,5 +2,14 @@ import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
 
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick label.
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
