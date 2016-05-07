22702b23a65400743039121e8347567ae2bf01da
crystalDf <chendong333@gmail.com>
2016-05-07 22:07:04 +0800

    Chapter 15 Data Visualization

--- random_walk.py
+++ random_walk.py
@@ -1,10 +1,11 @@
 from random import choice
 
-class RandomWalk():
+
+class RandomWalk:
     """A class to generate random walks."""
 
     def __init__(self, num_points=5000):
-        """Initiallize attributes of a walk."""
+        """Initialize attributes of a walk."""
         self.num_points = num_points
 
         # All walks start at (0, 0).
@@ -18,13 +19,8 @@ class RandomWalk():
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go and how far to go in that direction.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = get_step()
+            y_step = get_step()
 
             # Reject moves that go nowhere.
             if x_step != 0 or y_step != 0:
@@ -36,3 +32,10 @@ class RandomWalk():
 
                 self.x_values.append(next_x)
                 self.y_values.append(next_y)
+
+
+def get_step():
+    direction = choice([1, -1])
+    distance = choice([0, 1, 2, 3, 4])
+
+    return direction * distance
