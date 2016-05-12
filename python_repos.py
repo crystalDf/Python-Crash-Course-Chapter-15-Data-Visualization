64a1547cc97e82f4266e4de92db665d3ea2f2ce3
crystalDf <chendong333@gmail.com>
2016-05-12 22:28:10 +0800

    Chapter 15 Data Visualization

---
+++ python_repos.py
@@ -0,0 +1,58 @@
+import requests
+import pygal
+
+from pygal.style import LightColorizedStyle as LCS
+from pygal.style import LightenStyle as LS
+
+# Make an API call and store the response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code: " + str(r.status_code))
+
+# Store API response in a variable.
+response_dict = r.json()
+
+# Process results.
+print(response_dict.keys())
+
+print("Total repositories: " + str(response_dict['total_count']))
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print("Repositories returned: " + str(len(repo_dicts)))
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print()
+print("Keys: " + str(len(repo_dict)))
+for key in sorted(repo_dict.keys()):
+    print(key)
+
+names = []
+stars = []
+
+print()
+print("Selected information about each repository: ")
+
+for repo_dict in repo_dicts:
+
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+    print()
+    print('Name: ' + repo_dict['name'])
+    print('Owner: ' + repo_dict['owner']['login'])
+    print('Stars: ' + str(repo_dict['stargazers_count']))
+    print('Repository: ' + repo_dict['html_url'])
+    print('Created: ' + repo_dict['created_at'])
+    print('Updated: ' + repo_dict['updated_at'])
+    print('Description: ' + repo_dict['description'])
+
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
