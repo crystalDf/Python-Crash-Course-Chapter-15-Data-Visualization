a127cf6ec223a045f6c26dc491a3aa5fee0cc721
crystalDf <chendong333@gmail.com>
2016-05-13 23:09:51 +0800

    Chapter 15 Data Visualization

--- python_repos.py
+++ python_repos.py
@@ -30,6 +30,7 @@ for key in sorted(repo_dict.keys()):
 
 names = []
 stars = []
+plot_dicts = []
 
 print()
 print("Selected information about each repository: ")
@@ -38,6 +39,12 @@ for repo_dict in repo_dicts:
 
     names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
 
     print()
     print('Name: ' + repo_dict['name'])
@@ -50,9 +57,21 @@ for repo_dict in repo_dicts:
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+# chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
